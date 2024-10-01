59e267f54434754fe3909e3d7ff2e46a68231a9f
WahidMubarrat <wahidmubarrat20@gmail.com>
2024-10-01 11:46:20 +0600

    first commit

---
+++ Book.java
@@ -0,0 +1,61 @@
+public class Book {
+    private String title;
+    private String author;
+    private String genre;
+    private int pages;
+    private String dateRead;
+
+    // Constructor
+    public Book(String title, String author, String genre, int pages, String dateRead) {
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+        this.pages = pages;
+        this.dateRead = dateRead;
+    }
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    public String getDateRead() {
+        return dateRead;
+    }
+
+    public void setDateRead(String dateRead) {
+        this.dateRead = dateRead;
+    }
+
+
+
+    public String print() {
+        return  "title=" + title + '\'' + ", author='" + author + '\'' + ", genre='" + genre + '\'' + ", pages=" + pages + ", dateRead='" + dateRead + '\'' ;
+    }
+}
